a4d7c62b2ad27a182c8b480aa206566d634e0e2f
Bjørn Grevenkop-Castenskiold <bhgreven@pm.me>
2022-12-13 14:03:36 +0100

    Yeet test mapper og dirtree.py virker

--- dirtree.py
+++ dirtree.py
@@ -1,14 +1,10 @@
-
+import os
+import re
 
 dir = 'C:/Users/tardi/Desktop/ES7-Quantum/'
 
 gitlink = "https://github.com/UnknownDK/ES7-Quantum/"
-# Folder:
-# https://github.com/UnknownDK/ES7-Quantum/tree/master/qiskitCode
-# File:
-# https://github.com/UnknownDK/ES7-Quantum/blob/master/qiskitCode/measure00000.py
-import os
-import re
+
 
 def list_files(startpath):
     print("\dirtree{%")
@@ -17,21 +13,15 @@ def list_files(startpath):
             level = root.replace(startpath, '').count(os.sep) + 1
             if os.path.basename(root) != "":
                 level += 1
-            indent = ' ' * 4 * (level)
-            print('.' + str(level) + ' \href{' + gitlink + 'tree/master/' + os.path.basename(root) + '}{' + os.path.basename(root) + '/}.' )
-            #print('{}{}/'.format(indent, os.path.basename(root)))
-            subindent = ' ' * 4 * (level + 1)
+            folders = re.split(r' |/|\\', root)
+            for fold in reversed(folders):
+                if fold in dir:
+                    del(folders[folders.index(fold)])
+            folderpath = ""
+            for folder in folders:
+                folderpath += folder + "/"
+            print('.' + str(level) + ' \href{' + gitlink + 'tree/master/' + folderpath + '}{' + os.path.basename(root) + '/}.' )
             for f in files:
-                #print('.{} {}'.format(level + 1, f))
-                folders = re.split(r' |/|\\', root)
-                for fold in reversed(folders):
-                    if fold in dir:
-                        del(folders[folders.index(fold)])
-                folderpath = ""
-                for folder in folders:
-                    folderpath += folder + "/"
-                #print(folderpath)
-                print('.' + str(level + 1) + ' \href{' + gitlink + 'blob/master/' + folderpath + f + '}{' + f + '/}.' )
-                #print('{}{}'.format(subindent, f))
+                print('.' + str(level + 1) + ' \href{' + gitlink + 'blob/master/' + folderpath + f + '}{' + f + '}.' )
     print("}")
 list_files(dir)

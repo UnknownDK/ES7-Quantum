485b9896dca67f37a0feeaa78a8c7101780b1c1c
Bjørn Grevenkop-Castenskiold <bhgreven@pm.me>
2022-12-19 09:55:31 +0100

    Cleaned data

--- qiskitTestSetup/startupRun.py
+++
@@ -1,4 +0,0 @@
-from qiskit import IBMQ
-IBMQ.save_account('7306c1b7375bbe9174011e80ef2c9024832086f34bd70befbc112b262993a65aef4f167614171abb4e8acc7fc061092744719c2dd60353ffe2b0fdb7ccf3fbcc')
-
-#fra nu af kan "provider = "IBMQ.load_account()" bruges for at bruge min key
